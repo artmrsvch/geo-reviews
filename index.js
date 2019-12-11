9c939412622a057d5410890222db3301f6e1c9d9
Artem_Zabirov <qwertysairex@gmail.com>
2019-12-11 20:04:21 +0300

    variant 1

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 ymaps.ready(init);
 
 function init(){
-   
+    
     const modal = document.querySelector('#overlay');
-    let myPlacemark;
+    const otziv = document.querySelector('#otziv');
     let geoObj = new Array; 
     const create = document.querySelector('.create');
+    
     
     const myMap = new ymaps.Map('maps', {
         center: [55.7482921,37.5900027],
@@ -13,7 +14,10 @@ function init(){
     })
 
     myMap.events.add('click', function (e) {
-        let coords = e.get('coords'); 
+        if (create.innerHTML != '') {
+
+        } else {
+            let coords = e.get('coords'); 
         ymaps.geocode(coords)
             .then(function (res) {
                 let firstGeoObject = res.geoObjects.get(0);    
@@ -23,55 +27,75 @@ function init(){
                 reviewModal(adres, coords);
                 console.log(adres)
                 console.log(coords)
+                myMap.geoObjects.events.add('click', function (e) {
+                    console.log(e.get('target'))
+                   
+                })
             })
-
-
-        myPlacemark = createPlacemark(coords); //Создаем метку 
-
-        myMap.geoObjects.add(myPlacemark); 
-        myPlacemark.events.add('click', function (e) {
-            console.log(e.originalEvent.target.properties._data.balloonContent)
-            console.log('asdasd')
-        })
-
+        }
     });
     
     // Создание метки.
-    function createPlacemark(coords) {
-        return new ymaps.Placemark(coords,{
-           
-            balloonContent: 'цвет <strong>влюбленной жабы</strong>',
+    function createPlacemark(coords, adress, arrRev, placemark) {
+        if (placemark == undefined) {   //передаем метку
+            let temp = [];
+            
+            temp.push(arrRev);
+            return new ymaps.Placemark(coords,{
+                adressReview: adress,
+                reviews: temp,
         },{
             preset: 'islands#violetIcon',
         });
+        } else {
+            placemark.properties._data.reviews.push(arrRev)
+        }
     }
     
     // Определяем адрес по координатам (обратное геокодирование).
     
     function reviewModal(adress, coords, textRevi) {
-        const html = modal.innerHTML;
-        const template = Handlebars.compile(html);
-        const thisObj = {
-            position: adress
-        };
-        const psd = template(thisObj);
-        create.innerHTML = psd;
-        let arrRev = new Array;
-
-
-        create.addEventListener('click', (e)=>{
-            
-            if(e.target.className == 'close-reviews') {
+        let myPlacemark;
+        let arrRev = new Array;  //создаем массив отзывов
+        const template = Handlebars.compile(modal.innerHTML);
+        const psd = template({position: adress});
+        create.innerHTML = psd;   //открываем модалку с отзывами
+        
+        create.addEventListener('click', (e)=>{        //вешаем обработчик событий на модалку
+            if(e.target.className == 'close-reviews') {   //если клик по кнопке закрыть - закрываем модалку
                 create.innerHTML = '';
-            } else if (e.target.className == 'i-btn') {
-                arrRev.push(
+                myPlacemark = undefined;
+                arrRev = [];
+                
+
+            } else if (e.target.className == 'i-btn') {   //если клик по кнопке добавить  
+                console.log('КЛИК ПО ДОБАВВИТЬ')
+                arrRev.push(                              //пушим инпуты в массив отзывов
                     {
                         name: document.querySelector('.i-name').value,
                         place: document.querySelector('.i-place').value,
                         area: document.querySelector('.i-area').value
                     }
                 )
-                console.log(arrRev)  //тут массив отзывов
+                console.log(myPlacemark);
+                if (myPlacemark == undefined) {
+                    myPlacemark = 1;
+                    createPlacemark(coords, adress, arrRev); //создаем маркер с текущими отзывами и адресом
+                    myMap.geoObjects.add(myPlacemark);                     //добавлем маркер на карту
+                    myPlacemark.events.add('click', function (e) {
+                        console.log(e.originalEvent.target.properties._data.adressReview)
+                        console.log(e.originalEvent.target.properties._data.reviews)
+                    })
+                } else {
+                    console.log('ДАДАВАЙ НАХУЙ УЖЕ НАКОНЕЦ');
+                    myPlacemark.properties._data.reviews = arrRev;
+                }
+                
+                const acessObj = myPlacemark.properties._data;         //получаем доступ к хранилищу отзывов в метке
+                const templateOtz = Handlebars.compile(otziv.innerHTML);
+                const psdOtz = templateOtz(acessObj.reviews);
+                document.querySelector('.reviews').innerHTML = psdOtz; //добавляем отзыв в DOM узел
+
             }
         })  
         
@@ -90,10 +114,7 @@ function init(){
             return cluster;
         };*/
     myMap.geoObjects.add(clusterer);
-    myMap.geoObjects.events.add('click', (e)=>{
-
-        console.log(e)
-    });
+    
 
    
 }

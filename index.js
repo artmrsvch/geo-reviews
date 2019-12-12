11798e0beda8d11bc79ff327c8fc3776892ce08d
Artem_Zabirov <qwertysairex@gmail.com>
2019-12-13 00:37:04 +0300

    Complete 95%

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ function init(){
     const modal = document.querySelector('#overlay');
     const otziv = document.querySelector('#otziv');
     const create = document.querySelector('.create');
-    let myPlacemark;
+    let storage = localStorage;
+    let data;
     let thisAdress;
     let thisCoords;
     let placeObject = new Array;
     const customItemContentLayout = ymaps.templateLayoutFactory.createClass(
         '<div class="ballon_place ballon_our"><strong>{{ properties.reviews.place|raw }}</strong></div>' +
         '<a href="#" id="links" class="ballon_adres ballon_our">{{ properties.adressReview|raw }}</a>' +
-        '<div class="ballon_area ballon_our">{{ properties.reviews.area|raw }}</div>', {
+        '<div class="ballon_area ballon_our">{{ properties.reviews.area|raw }}</div>' +
+        '<div class="ballon_time ballon_our">{{ properties.reviews.date|raw }}</div>', {
             build: function () {
                 customItemContentLayout.superclass.build.call(this);
                 document.querySelector('#links').addEventListener('click', this.onCounterClick);
@@ -29,14 +31,19 @@ function init(){
                 myMap.balloon.close();
             }
         }
-    ); 
+    );
+    try {
+        data = JSON.parse(storage.data);
+    } catch (e) {
+        data = undefined;
+    }
     const myMap = new ymaps.Map('maps', {
         center: [55.7482921,37.5900027],
         zoom: 15 
     })
+    
     myMap.events.add('click', function (e) {
         if (create.innerHTML != '') {
-
         } else {
             getModal (e);
         }
@@ -64,12 +71,25 @@ function init(){
             })
     }
     function createPlacemark(coords, adress, arrRev) {
-        return new ymaps.Placemark(coords,{
+        let comentDate = formatDateBaloon(new Date);
+        let mark = new ymaps.Placemark(coords,{
             adressReview: adress,
             reviews: arrRev,
+            date: comentDate
         },{
             preset: 'islands#violetIcon',
         });
+        placeObject.push({
+            coord: coords,
+            adres: adress,
+            review: arrRev,
+            date: comentDate
+        });
+        storage.data = JSON.stringify(placeObject);
+        myMap.geoObjects.add(mark);
+        clusterer.add(mark);
+
+        return mark
     }   
     function reviewModal(adress, coords) {
         const template = Handlebars.compile(modal.innerHTML);
@@ -81,16 +101,14 @@ function init(){
     create.addEventListener('click', (e)=>{
         if(e.target.className == 'close-reviews') {
             create.innerHTML = '';
-            myPlacemark = undefined;
-        } else if (e.target.className == 'i-btn') {          
-            myPlacemark = createPlacemark(thisCoords, thisAdress, {
+        } else if (e.target.className == 'i-btn') {      
+                let nowDate = formatDateModal(new Date);
+                createPlacemark(thisCoords, thisAdress, {
                 name: document.querySelector('.i-name').value,
                 place: document.querySelector('.i-place').value,
-                area: document.querySelector('.i-area').value
+                area: document.querySelector('.i-area').value,
+                dateModal: nowDate
             });
-            placeObject.push(myPlacemark);
-            myMap.geoObjects.add(myPlacemark);
-            clusterer.add(myPlacemark);
             eachLi(thisAdress);
         } 
     })
@@ -108,15 +126,41 @@ function init(){
     }
     myMap.geoObjects.events.add('click', function (e) {
         const targ = e.get('target');
+        console.log(targ);
         if (targ.properties._data.geoObjects) {
             
         } else {
-            myPlacemark = targ;
             reviewModal(targ.properties._data.adressReview, e.get('coords'));
             eachLi(targ.properties._data.adressReview);
         }
     })
-    
+    function formatDateBaloon(date) {
+
+        let dd = date.getDate();
+        if (dd < 10) dd = '0' + dd;
+      
+        let mm = date.getMonth() + 1;
+        if (mm < 10) mm = '0' + mm;
+      
+        let yy = date.getFullYear();
+        let se = date.getSeconds();
+        let mi = date.getMinutes();
+        let hr = date.getHours();
+        
+        return yy + '.' + mm + '.' + dd + ' ' + hr +':'+ mi +':'+ se;
+    }
+    function formatDateModal(date) {
+
+        let dd = date.getDate();
+        if (dd < 10) dd = '0' + dd;
+      
+        let mm = date.getMonth() + 1;
+        if (mm < 10) mm = '0' + mm;
+      
+        let yy = date.getFullYear();
+
+        return dd + '.' + mm + '.' + yy;
+    }
     let clusterer = new ymaps.Clusterer({
         clusterDisableClickZoom: true,
         clusterOpenBalloonOnClick: true,
@@ -128,5 +172,10 @@ function init(){
         clusterBalloonContentLayoutHeight: 130,
         clusterBalloonPagerSize: 5
     });
-    myMap.geoObjects.add(clusterer);   
+    myMap.geoObjects.add(clusterer);
+    if (data) {
+        data.forEach(thismark => {
+            createPlacemark (thismark.coord, thismark.adres, thismark.review);
+        })
+    }
 }
